8d8532799dd47bb03400174d7667b5737dc5eb84
Joshua R Miller <joshua.ross.miller@gmail.com>
2018-06-14 14:28:26 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -28,6 +28,6 @@ def now_serving(array)
     puts "There is no one in line."
   else
     person = array.shift
-    puts "Currently serving #{person}"
+    puts "Currently serving #{person}."
   end
 end
